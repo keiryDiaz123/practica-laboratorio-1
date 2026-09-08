32b1076fb4f3e92c0e66bc4cf47da6c7e199e4bd
keiryDiaz123 <keirydiaz2019@gmail.com>
2026-09-07 20:22:55 -0600

    Agregar carpeta practica 2 y sus contenidos

--- practica 3/ejer2.js
+++ practica 3/ejer2.js
@@ -5,7 +5,7 @@
 
 const readline = requiere('readline');
 
-cont kd = readline.createIerface({
+const kd = readline.createIerface({
     input:process.stidin,
     output:process.stdout
 
---
+++ practica 3/ejer3.js
@@ -0,0 +1,25 @@
+//Validar calificación: Solicite una calificación 
+// (0-100). Muestre si aprobó (>=60) o reprobó (<60).
+
+
+const readline = requiere('readline');
+
+cont kd = readline.createIerface({
+    input:process.stidin,
+    output:process.stdout
+
+})
+
+kd.question("ingrese su calificacion:",(calificacion)=>{
+    if(calificacion>=0 && calificacion>=100){
+          if(calificacion>=60){
+            console.log("felicidades usted aprobo  ")
+          }else{
+            console.log("lo centimos pero usted noa aprobo")
+          }
+    }else{
+        console.log("su calificacion tiene que estar en el rango de (0-100")
+    }
+    kd.close();
+})
+
