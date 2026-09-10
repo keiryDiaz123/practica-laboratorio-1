3ebc909089e41ac3f7336d71a4ef16e5c40be49d
keiryDiaz123 <keirydiaz2019@gmail.com>
2026-09-09 20:10:07 -0600

    Borrar totalmente la carpeta practica 1

--- practica 1/practica 1/ejercicio3.js
+++
@@ -1,6 +0,0 @@
-let base = 40;
-let altura = 1.80;
-let area = (base*altura)
-
-console.log(`la base de su rectangulo es: ${base} y su altura es: ${altura}`);
-console.log(`el area de su rectangulo es: ${area}m^2`);
--- practica 1/practica 1/ejercicio7.js
+++
@@ -1,7 +0,0 @@
-let  salario = 400;
-let bonificacion = salario*0.15;
-let salarioTotal = salario+bonificacion;
-
-console.log(`su bonificacion es: ${bonificacion}`);
-console.log(`su salario sin bonificacion es: ${salario}`);
-console.log(`su salario total con bonificacion es: ${salarioTotal}`);
