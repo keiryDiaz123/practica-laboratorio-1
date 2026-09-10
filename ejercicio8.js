8f598737963ae6b1b9ae501c420ff62c8b74da94
keiryDiaz123 <keirydiaz2019@gmail.com>
2026-09-09 19:43:55 -0600

    Eliminar ejercicios sueltos de la raiz

--- ejercicio8.js
+++
@@ -1,7 +0,0 @@
-let precio = 40;
-let descuento = producto*0.20
-let productodescuento = precio-descuento
-
-console.log(`el precio original: ${precio}`);
-console.log(`   descuento : ${descuento}`);
-console.log(`precio final : ${productodescuento}`);
